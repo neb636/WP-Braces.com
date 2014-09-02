ed7cd100b187fc357919f6a77dc757eb611a563e
Nick <neb636@gmail.com>
2014-09-01 21:02:38 -0400

    Refactored BuilderRoutes into seprate methods

--- app.rb
+++ app.rb
@@ -24,54 +24,25 @@ class BuilderRoutes < Sinatra::Base
 
   post '/basetheme' do
     begin
-      theme_name = params[:theme_name].gsub('*/', '')
-      author_name = params[:author_name].gsub('*/', '')
-      author_url = params[:author_url].gsub('*/', '')
-      theme_url = params[:theme_url].gsub('*/', '')
-      description = params[:description].gsub('*/', '')
-      language = params[:language_support]
-      sass = params[:sass]
-      compass = params[:compass]
-      gulp = params[:gulp]
-      # custom_post_types = params[:custom_post_types]
-      # custom_post_types_number = params[:cpt_number]
+      set_variables
 
-      # Be naughty and polute the global namespace for convienence
-      $base_theme_directory = 'public/temp/theme_1/'
-      temp_number = 1
-
-      # Change value of $base_theme_directory until the file name does not appear
-      until !File.exist?($base_theme_directory)
-        temp_number = temp_number + 1
-        $base_theme_directory = 'public/temp/theme_' + temp_number.to_s + '/'
-      end
+      set_base_theme_directory
 
       # Copy files to temp folder
       Builder.temp
 
-      ## Load up all parameter methods
-      Questions.language_support(language)
-      Questions.sass_support(sass)
-      Questions.compass_support(sass, compass)
-      Questions.gulp_support(gulp)
-      Questions.theme_name_write(theme_name)
-      Questions.author_name_write(author_name)
-      Questions.author_url_write(author_url)
-      Questions.theme_url_write(theme_url)
-      Questions.project_prefix_write(theme_name)
-      Questions.theme_description_write(description)
+      # Load up all parameter methods
+      load_questions
 
       # Zip and save as variable
-      Builder.zip_file(temp_number)
-      content_type(:zip)
-      content = File.read("public/temp/builder_theme#{temp_number}.zip")
+      zip_and_send_files
 
       # Delete temp files created
-      File.delete("public/temp/builder_theme#{temp_number}.zip")
+      File.delete("public/temp/builder_theme#{@temp_number}.zip")
       FileUtils.rm_rf($base_theme_directory)
 
       # Return the content so it starts auto download
-      content
+
 
     # If there is an error send users to the error page and send email about
     # the error to me
@@ -79,5 +50,52 @@ class BuilderRoutes < Sinatra::Base
       FileUtils.rm_rf($base_theme_directory)
       erb :error
     end
+  end
+
+  private
+
+  def set_variables
+    @theme_name = params[:theme_name].gsub('*/', '')
+    @author_name = params[:author_name].gsub('*/', '')
+    @author_url = params[:author_url].gsub('*/', '')
+    @theme_url = params[:theme_url].gsub('*/', '')
+    @description = params[:description].gsub('*/', '')
+    @language = params[:language_support]
+    @sass = params[:sass]
+    @compass = params[:compass]
+    @gulp = params[:gulp]
+    # @custom_post_types = params[:custom_post_types]
+    # @custom_post_types_number = params[:cpt_number]
+  end
+
+  def set_base_theme_directory
+    # Be naughty and polute the global namespace for convienence
+    $base_theme_directory = 'public/temp/theme_1/'
+    @temp_number = 1
+
+    # Change value of $base_theme_directory until the file name does not appear
+    until !File.exist?($base_theme_directory)
+      @temp_number = @temp_number + 1
+      $base_theme_directory = 'public/temp/theme_' + @temp_number.to_s + '/'
+    end
+  end
+
+  def zip_and_send_files
+    Builder.zip_file(@temp_number)
+    content_type(:zip)
+    File.read("public/temp/builder_theme#{@temp_number}.zip")
+  end
+
+  def load_questions
+    Questions.language_support(@language)
+    Questions.sass_support(@sass)
+    Questions.compass_support(@sass, @compass)
+    Questions.gulp_support(@gulp)
+    Questions.theme_name_write(@theme_name)
+    Questions.author_name_write(@author_name)
+    Questions.author_url_write(@author_url)
+    Questions.theme_url_write(@theme_url)
+    Questions.project_prefix_write(@theme_name)
+    Questions.theme_description_write(@description)
   end
 end
