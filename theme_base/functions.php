5cf3b1dc354db1067ab941910d33243218487bf8
Nick <neb636@gmail.com>
2014-12-19 12:39:27 -0500

    Removed Custom Header commented out code and unneeded comment.

--- theme_base/functions.php
+++ theme_base/functions.php
@@ -113,15 +113,6 @@ function {%= prefix %}_scripts() {
 add_action( 'wp_enqueue_scripts', '{%= prefix %}_scripts' );
 
 /**
- * Add Custom Classes
- */
-
-/**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
