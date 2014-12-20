c8f10a75845a454eaecfcb82095a6fdee9428d7d
Nick <neb636@gmail.com>
2014-12-20 18:04:22 -0500

    Added in comment for angular section

--- public/js/form.js
+++ public/js/form.js
@@ -3,6 +3,9 @@ var wp_braces = angular.module('wp-braces', []);
 wp_braces.controller('PostTypesCtrl', function ($scope) {
 	var self = this;
 
+	// Asks user how many post types needed. If amount is more than 15 display an
+	// error otherwise create new array with number of cpts needed and do ng-repeat
+	// to simulate doing something like _.times in Angular.
 	self.update_amount = function() {
 		self.amount = parseInt(self.amount);
 
